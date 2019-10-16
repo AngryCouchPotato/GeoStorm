cf0f2e3ceddc599c304c5e68c752d86763a785d2
AngryCouchPotato <alexeykolcov@gmail.com>
2019-10-16 09:37:47 +0200

    -delete CrossOrigin -add Tomcat provided -decrease java version

--- src/main/java/com/kaa/geostorm/controller/GeoNamesController.java
+++ src/main/java/com/kaa/geostorm/controller/GeoNamesController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin("http://localhost:3000")
 @RequestMapping("/geonames")
 @AllArgsConstructor
 public class GeoNamesController {
